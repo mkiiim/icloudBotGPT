254d540674aaa64a3a8697e6fe037dcdc51d404a
mkiiim <85082256+mkiiim@users.noreply.github.com>
2024-06-06 16:34:33 -0400

    switched from OpenAI completion to assistant

--- chat_functionality.py
+++ chat_functionality.py
@@ -1,19 +1,51 @@
 from openai import OpenAI
 from config import *
+import time
 import yaml
 
 client = OpenAI(api_key=APIKEY_OPENAI)
 
 # Change model and parameters as needed
 # Going forward, create a LLM completion object for each LLMN model you want to use. This will allow you to use multiple models in the same script.
-def ChatGPT_completion(conversation):
-    response = client.chat.completions.create(
+def ChatGPT_completion(prompt_instructions, message_history):
+    assistant = client.beta.assistants.create(
         model="gpt-4o",
-        messages=conversation,
-        max_tokens=1000,
+        name = "{os.getenv('USER')}_assistant",
+        instructions = prompt_instructions,
+        # max_tokens=1000,
         temperature=0.2)
-        
-    return response
+    
+    # imessage_id will be invalid in the message_history object. Remove it.
+    for m in message_history:
+        m.pop('imessage_id', None)
+
+    # create thread object
+    thread = client.beta.threads.create(
+        messages=message_history,
+    )
+
+    # create the run object
+    run = client.beta.threads.runs.create(
+        thread_id = thread.id,
+        assistant_id = assistant.id,
+        model="gpt-4o",
+    )
+
+    # check for run completion in a loop. *** TODO *** Need to account for cancelled_at and failed_at
+    while run.completed_at is None:
+        run = client.beta.threads.runs.retrieve(
+            run_id=run.id,
+            thread_id=thread.id,
+            # assistant_id=assistant.id
+        )
+        time.sleep(1)
+
+    # get the reply from threads.messages.list
+    reply = client.beta.threads.messages.list(
+        thread_id=thread.id,
+    )
+
+    return reply.data[0].content[0].text.value
 
 def load_prompts(file_path):
     with open(file_path, 'r') as file:
@@ -22,19 +54,19 @@ def load_prompts(file_path):
 
 def build_prompt(conversation, system_prompt="You are a helpful assistant. Provide a response to the user(s)."):
     completion = []
-    completion.append({"role": "system", "content": system_prompt})
+    completion.append({"role": "assistant", "content": system_prompt})
     completion.extend(conversation)
     return completion
 
 def build_prompt_response(conversation, system_prompt="You are a helpful assistant. Provide a response to the user(s)."):
     completion = []
-    completion.append({"role": "system", "content": system_prompt})
+    # completion.append({"role": "assistant", "content": system_prompt})
     completion.extend(conversation)
     return completion
 
 def build_prompt_analysis(conversation, system_prompt="Analyze the message thread but do not generate a response to the user. Generate only the analysis."):
     completion = []
-    completion.append({"role": "system", "content": system_prompt})
+    completion.append({"role": "assistant", "content": system_prompt})
     completion.extend(conversation)
     return completion
 

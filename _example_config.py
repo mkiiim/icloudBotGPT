c2a3c6a7cbf1883488683f946edf7e1dabec7ba4
mkiiim <85082256+mkiiim@users.noreply.github.com>
2024-06-25 15:50:01 -0400

    DB_PATH to MESSAGE_DB_PATH in anticipation of upcoming RAG related changes

--- _example_config.py
+++ _example_config.py
@@ -1,11 +1,21 @@
 import openai
 import os
 
-# Define your API keys in environment variables and access them here
 APIKEY_OPENAI = os.getenv('APIKEY_OPENAI')
-openai.api_key = APIKEY_OPENAI
+EMBEDDING_ENGINE = "text-embedding-ada-002"
+EMBEDDING_DIMENSION = 1536
+MAX_TOKENS_PER_FILE = 128
 
-DB_PATH = f"/Users/{os.getenv('USER')}/Library/Messages/chat.db"
+# *YOUR* username, the one to which this bot will be subservient
+# This is *NOT* the username of the bot!
+# This user will need to have an account on the *SAME MACHINE* as the bot
+# in order to more easily facilitate access to this users' documents
+# and application data (e.g. calendars, notes, etc.). This will more easily
+# allow for "personalized context" that the bot can use to generate more
+# relevant responses.
+USERNAME = "<your_username>"
+
+MESSAGE_DB_PATH = f"/Users/{os.getenv('USER')}/Library/Messages/chat.db"
 PROMPTS_FILE = f"/Users/{os.getenv('USER')}/Projects/icloudBotGPT/prompts.yml"
 
 # File to store last message processed
@@ -25,3 +35,6 @@ USE_ASSISTANT = False
 
 # file size limit for attachments, in bytes
 FILE_SIZE_LIMIT = 3 * 1024 * 1024
+
+# Root directory for file access
+ROOT_DIR = f"/Users/{os.getenv('USER')}/"
